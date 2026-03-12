6a237e0965dacd842c8f7ea615ae3306332fadf9
Carlier Benjamin <benjamincarlier.svt@gmail.com>
2026-03-12 17:40:41 -0400

    Création des attributs de classe Pays

--- TP2-AEK-BC/src/livres/Pays.java
+++ TP2-AEK-BC/src/livres/Pays.java
@@ -1,4 +1,14 @@
 package livres;
 
 public class Pays {
+    //Constantes
+    public static final String NOM_PAYS_DEFAUT = "nom defaut";
+    public static final String CODE_PAYS_DEFAUT = "DEF";
+    //Attributs
+    private String nomPays = NOM_PAYS_DEFAUT;
+    private String codePays = CODE_PAYS_DEFAUT;
+
+
+
+    //fin classe
 }
