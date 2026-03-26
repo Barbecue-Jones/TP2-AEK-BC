4715623d6f9e7d3fad45b6c481af200b4537bde2
Amjad El Kharraz <amjadelkharraz11@gmail.com>
2026-03-26 18:51:29 -0400

    Modification des ouvrages dans la classe TestOuvrage

--- TP2-AEK-BC/src/tests/TestOuvrage.java
+++ TP2-AEK-BC/src/tests/TestOuvrage.java
@@ -5,10 +5,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import livres.Ouvrage;
-import livres.Auteur;
-import livres.Pays;
-import livres.Serie;
+import livres.*;
 
 /**
  * CoursPOO 1
@@ -43,8 +40,8 @@ public class TestOuvrage {
         Serie bible = new Serie("vieuxTestatment", new ArrayList<Ouvrage>());
 
         //creation ouvrage
-        Ouvrage ouvrage1 = new Ouvrage("ouvrage1", john, Ouvrage.TYPE_DEFAULT);
-        Ouvrage ouvrage2 = new Ouvrage("ouvrage2", albertine, Ouvrage.TYPE_DEFAULT);
+        Ouvrage ouvrage1 = new OuvragePapier("Titre assez long", null, LocalDate.now(), 2, 32);
+        Ouvrage ouvrage2 = new OuvragePapier("Titre assez long", null, LocalDate.now(), 2, 32);
 
 
         //ajout d' ouvrages aux series
@@ -68,18 +65,18 @@ public class TestOuvrage {
 
         //Voici une partie des tests! Il faut en ajouter, pour les fonctionnalités non testées!
         System.out.println("-----Test des constructeurs d'ouvrage et des diverses validations-----------");
-        Ouvrage livreA = new Ouvrage("Titre assez long", john);
+        Ouvrage livreA = new OuvragePapier("Titre assez long", john, LocalDate.now(), 1, 23);
         System.out.println(livreA);
-        Ouvrage livreB = new Ouvrage("Ti", john);
+        Ouvrage livreB = new OuvragePapier("Ti", john, LocalDate.now(), 1, 23);
         System.out.println(livreB);
-        Ouvrage livreC = new Ouvrage(null, john);
+        Ouvrage livreC = new OuvragePapier(null, john, LocalDate.now(), 1, 23);
         System.out.println(livreC);
 
         //bibliotheque.Auteur null et valeur par défaut de l'bibliotheque.Auteur
-        Ouvrage livreA1 = new Ouvrage("Titre assez long", null);
+        Ouvrage livreA1 = new OuvragePapier("Titre assez long", null, LocalDate.now(), 2, 32);
         System.out.println(livreA1);
         //bibliotheque.Auteur fonctionnel
-        Ouvrage livre1 = new Ouvrage("Tout va bien", albertine);
+        Ouvrage livre1 = new OuvragePapier("Tout va bien", albertine, LocalDate.now(), 1, 23);
         System.out.println(livre1);
 
         //Date null et valeur par défaut de la date
@@ -89,15 +86,15 @@ public class TestOuvrage {
         System.out.println(livre1);
 
         //Test de la validation sur le nb d'exemplaires (valide et non valide)
-        Ouvrage livre2 = new Ouvrage("Tout va bien", albertine, Ouvrage.Format.AUDIO, LocalDate.now(), -10);
+        Ouvrage livre2 = new OuvrageAudio("Tout va bien", albertine, LocalDate.now(), 10, 34, FormatAudio.NUMERIQUE);
         System.out.println(livre2);
 
-        livre2 = new Ouvrage("Tout va bien", albertine, Ouvrage.Format.PAPIER, LocalDate.now(), 20);
+        livre2 = new OuvragePapier("Tout va bien", albertine, LocalDate.now(), 20, 20);
         System.out.println(livre2);
 
         System.out.println("\n-----Tests des méthodes acheter et vendre-----------");
 
-        Ouvrage livre3 = new Ouvrage("Musique du hasard", new Auteur("Paul", "Auster", null), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
+        Ouvrage livre3 = new OuvragePapier("Tout va bien", albertine, LocalDate.now(), 1, 23);
         System.out.println(livre3);
 
         livre3.acheter(5);
@@ -109,14 +106,14 @@ public class TestOuvrage {
         System.out.println("On peut vendre 10 livres? " + livre3.vendre(10));
         System.out.println(livre3);
 
-        Ouvrage livre4 = new Ouvrage("Test", new Auteur("A", "B", null), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
+        Ouvrage livre4 = new OuvragePapier("Titre assez long", null, LocalDate.now(), 2, 32);
 
         System.out.println("\n-----Tests de la  méthode equals()-----------");
         //Deux ouvrages égaux
-        Ouvrage livre5 = new Ouvrage("Test", new Auteur("A", "B", null), Ouvrage.Format.PAPIER, null, 5);
-        Ouvrage livre6 = new Ouvrage("Test", new Auteur("A", "B", null), Ouvrage.Format.PAPIER, LocalDate.now(), 10);
+        Ouvrage livre5 = new OuvragePapier("Titre assez long", null, LocalDate.now(), 2, 32);
+        Ouvrage livre6 = new OuvragePapier( "Titre assez long", null, LocalDate.now(), 2, 32);
         //Un qui ne l'est pas
-        Ouvrage livre7 = new Ouvrage("Test", new Auteur("Z", "B", null), Ouvrage.Format.PAPIER, LocalDate.now(), 5);
+        Ouvrage livre7 = new OuvragePapier( "Titre assez long", null, LocalDate.now(), 2, 32);
 
         System.out.println("Test de la méthode equals d'bibliotheque.Ouvrage:" + livre4.equals(livre5));
         System.out.println("Test de la méthode equals d'bibliotheque.Ouvrage:" + livre4.equals(livre6));
