05339c4102cf2e65e2122148df1f36d395d31edc
Carlier Benjamin <benjamincarlier.svt@gmail.com>
2026-03-26 18:02:54 -0400

    Modification classe OuvrageVideo avec extend Ouvrage

--- TP2-AEK-BC/src/livres/OuvrageVideo.java
+++ TP2-AEK-BC/src/livres/OuvrageVideo.java
@@ -1,14 +1,18 @@
 package livres;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
-public class OuvrageVideo {
+public class OuvrageVideo extends Ouvrage{
     private int dureeEnMinutes;
     private double taille;
 
-    public OuvrageVideo(int dureeEnMinutes, double taille) {
-        this.dureeEnMinutes = dureeEnMinutes;
-        this.taille = taille;
+    public OuvrageVideo(String titre, Auteur auteur, LocalDate date, int nombreExemplaires) {
+        super(titre, auteur, date, nombreExemplaires);
+    }
+
+    public OuvrageVideo(String titre, Auteur auteur) {
+        super(titre, auteur);
     }
 
     public int getDureeEnMinutes() {
@@ -30,13 +34,14 @@ public class OuvrageVideo {
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
         OuvrageVideo that = (OuvrageVideo) o;
         return dureeEnMinutes == that.dureeEnMinutes && Double.compare(taille, that.taille) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(dureeEnMinutes, taille);
+        return Objects.hash(super.hashCode(), dureeEnMinutes, taille);
     }
 
     @Override
@@ -44,6 +49,6 @@ public class OuvrageVideo {
         return "OuvrageVideo{" +
                 "dureeEnMinutes=" + dureeEnMinutes +
                 ", taille=" + taille +
-                '}';
+                "} " + super.toString();
     }
 }
